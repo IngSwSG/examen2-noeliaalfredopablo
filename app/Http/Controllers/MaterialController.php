766940740c3247b154ef99a8074979a2b6177f03
noelia.alvaradomoreno <noelia.alvaradomoreno@ucr.ac.cr>
2024-07-03 18:09:50 -0600

    Creacion de metodo store del controller material

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class MaterialController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        // Validar los datos de entrada
+        $request->validate([
+            'codigo' => 'required|string|max:255',
+            'unidadMedida' => 'required|string|max:255',
+            'descripcion' => 'required|string|max:255',
+            'ubicacion' => 'required|string|max:255',
+            'idCategoria' => 'required|exists:categorias,id',
+        ]);
+
+        // Crear un nuevo material
+        $material = Material::create([
+            'codigo' => $request->codigo,
+            'unidadMedida' => $request->unidadMedida,
+            'descripcion' => $request->descripcion,
+            'ubicacion' => $request->ubicacion,
+            'idCategoria' => $request->idCategoria,
+        ]);
+
+        return response()->json($material, 201);
+    }
 }
