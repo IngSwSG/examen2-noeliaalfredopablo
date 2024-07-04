2b2099d762cc10d4e0cd27fcc5e93b9154401cef
AntiquityOfAll <pablocheli2001@gmail.com>
2024-07-03 18:23:54 -0600

    Implementación de routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+// Ruta para insertar un material
+Route::post('/materiales', [MaterialController::class, 'store']);
+
+// Ruta para actualizar un material
+Route::put('/materiales/{id}', [MaterialController::class, 'update']);
+
+// Ruta para obtener la lista de materiales
+Route::get('/materiales', [MaterialController::class, 'index']);
